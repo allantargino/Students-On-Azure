390b088219cec128e210bca603a026bb05603e60
Allan Holanda Targino <allan_targino@hotmail.com>
2015-11-27 13:39:03 -0200

    App added

---
+++ mywebapp/mywebapp/home.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="mywebapp.home" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server"><title>My Page!</title></head>
+<body>
+    <h1>Web App</h1>
+    <h2>Cadastrar cliente:</h2>
+    <form runat="server">
+        <label for="txtNome">Nome:</label>
+        <input runat="server" type="text" name="Nome" id="txtNome" />
+        <br />
+        <label for="txtSobrenome">Sobrenome:</label>
+        <input runat="server" type="text" name="Sobrenome" id="txtSobrenome" />
+        <br />
+        <label for="txtIdade">Idade:</label>
+        <input runat="server" type="number" name="Idade" id="txtIdade" />
+        <br />
+        <label for="cmbSexo">Sexo:</label>
+        <select runat="server" name="Sexo" id="sltSexo">
+            <option value=""></option>
+            <option value="F">F</option>
+            <option value="M">M</option>
+        </select>
+        <br />
+        <label for="txtEstado">Estado:</label>
+        <input runat="server" type="text" name="Estado" id="txtEstado" />
+        <br />
+        <asp:Button runat="server" Text="Cadastrar" OnClick="OnClick" />
+    </form>
+    <p runat="server" id="msg"></p>
+    <h2>Dados:</h2>
+    <asp:Table ID="tblDados" runat="server" />
+</body>
+</html>
---
+++ mywebapp/mywebapp/home.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Web.UI.WebControls;
+using MySql.Data.MySqlClient;
+
+namespace mywebapp
+{
+    public partial class home : System.Web.UI.Page
+    {
+        //TODO: Adicione sua conexão aqui
+        MySqlConnection connection = new MySqlConnection("SUA STRING DE CONEXÃO AQUI");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+                atualizar();
+        }
+
+        protected void OnClick(object sender, EventArgs e)
+        {
+            try
+            {
+                string cmd = "";
+                MySqlCommand command = new MySqlCommand();
+                command.Connection = connection;
+
+                cmd += "SET @@auto_increment_increment=1;";
+                cmd += "INSERT INTO tblClientes(nome, sobrenome,idade,sexo,estado) VALUES (@nome, @sobrenome, @idade, @sexo, @estado);";
+
+                command.CommandText = cmd;
+                command.Parameters.AddWithValue("@nome", txtNome.Value);
+                command.Parameters.AddWithValue("@sobrenome", txtSobrenome.Value);
+                command.Parameters.AddWithValue("@idade", txtIdade.Value);
+                command.Parameters.AddWithValue("@sexo", sltSexo.Value);
+                command.Parameters.AddWithValue("@estado", txtEstado.Value);
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+
+                atualizar();
+                limpar();
+                msg.InnerText = "Sucesso!";
+            }
+            catch (Exception ex)
+            {
+                msg.InnerText = "Erro:" + ex.Message;
+            }
+        }
+
+        private void atualizar()
+        {
+            try
+            {
+                tblDados.Rows.Clear();
+                MySqlCommand command = new MySqlCommand("SELECT * FROM tblClientes;", connection);
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    var row = new TableRow();
+                    TableCell[] cells = new TableCell[6];
+                    for (int i = 0; i < 6; i++)
+                    {
+                        cells[i] = new TableCell();
+                        cells[i].Text = reader[i].ToString();
+                    }
+                    row.Cells.AddRange(cells);
+                    tblDados.Rows.Add(row);
+                }
+                reader.Close();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                msg.InnerText = "Erro:" + ex.Message;
+            }
+        }
+
+        private void limpar()
+        {
+            txtNome.Value = "";
+            txtSobrenome.Value = "";
+            txtIdade.Value = "";
+            sltSexo.Value = "";
+            txtEstado.Value = "";
+        }
+    }
+}
